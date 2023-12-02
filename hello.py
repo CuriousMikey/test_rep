697b0a553db053964fab3ca581f07f38122404b6
CuriousMikey <michael.turyanitca@gmail.com>
2023-12-02 04:24:50 +0100

    Added age calculation

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-name = input("Please, enter your name ")
+name = input("Please, enter your name: ")
+birth_year = input("Please, enter your birth year: ")
+age = 2023 - birth_year
+
 print(f"Hello, {name}!")
+print(f"You are {age} years old")
