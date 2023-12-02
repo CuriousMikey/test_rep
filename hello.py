c32f88a1fcbb8547a83a88d8863168bf1923af17
CuriousMikey <michael.turyanitca@gmail.com>
2023-12-02 04:38:03 +0100

    last name digits calc

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 name = input("Please, enter your name: ")
-birth_year = input("Please, enter your birth year: ")
+birth_year = int(input("Please, enter your birth year: "))
 age = 2023 - birth_year
 
 print(f"Hello, {name}!")
 print(f"You are {age} years old")
+
+last_name = input("Please, enter your last name: ")
+last_name_lenght =len(last_name)
+
+print(f"Your last name has {last_name_lenght} letters")
